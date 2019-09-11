bf7a323f802a05158c79b11b03cd31b3c8bf2cba
Silas Rech <silas@panascais.net>
2019-09-11 18:57:16 +0200

    Move tests to TypeScript

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import 'app-module-path/cwd';
+/* tslint:disable object-literal-sort-keys */
 
 import test from 'ava';
 
@@ -19,256 +19,264 @@ const gaben = {
         universe1: 'STEAM_1:0:11101',
     },
     steam3: '[U:1:22202]',
+};
+
+interface BaseCheckObject {
+    universe: Universe;
+    type: Type;
+    instance: Instance;
+    accountid: number;
+    format: string;
 }
 
 test('Parameterless construction', (t) => {
-    const should = {
-        'universe':  Universe.INVALID,
-        'type':      Type.INVALID,
-        'instance':  Instance.ALL,
-        'accountid': 0,
-        'format':    'none',
+    const should: BaseCheckObject = {
+        universe:  Universe.INVALID,
+        type:      Type.INVALID,
+        instance:  Instance.ALL,
+        accountid: 0,
+        format:    'none',
     };
 
     const id = new ID();
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.false(id.isValid(), 'ID should not be valid');
 });
 
 test('fromAcccountID construction', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.INDIVIDUAL,
-        'instance':  Instance.DESKTOP,
-        'accountid': gaben.accountid,
-        'format':    'accountid',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.INDIVIDUAL,
+        instance:  Instance.DESKTOP,
+        accountid: gaben.accountid,
+        format:    'accountid',
     };
 
     const id = fromAccountID(gaben.accountid);
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('Steam2ID construction | Universe 0', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.INDIVIDUAL,
-        'instance':  Instance.DESKTOP,
-        'accountid': gaben.accountid,
-        'format':    'steam2',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.INDIVIDUAL,
+        instance:  Instance.DESKTOP,
+        accountid: gaben.accountid,
+        format:    'steam2',
     };
 
     const id = new ID(gaben.steam2.universe0);
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('Steam2ID construction | Universe 1', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.INDIVIDUAL,
-        'instance':  Instance.DESKTOP,
-        'accountid': gaben.accountid,
-        'format':    'steam2',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.INDIVIDUAL,
+        instance:  Instance.DESKTOP,
+        accountid: gaben.accountid,
+        format:    'steam2',
     };
 
     const id = new ID(gaben.steam2.universe1);
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('Steam3ID construction | Individual', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.INDIVIDUAL,
-        'instance':  Instance.DESKTOP,
-        'accountid': gaben.accountid,
-        'format':    'steam3',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.INDIVIDUAL,
+        instance:  Instance.DESKTOP,
+        accountid: gaben.accountid,
+        format:    'steam3',
     };
 
     const id = new ID(gaben.steam3);
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('Steam3ID construction | Gameserver', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.GAMESERVER,
-        'instance':  Instance.ALL,
-        'accountid': 31,
-        'format':    'steam3',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.GAMESERVER,
+        instance:  Instance.ALL,
+        accountid: 31,
+        format:    'steam3',
     };
 
     const id = new ID('[G:1:31]');
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('Steam3ID construction | Anonymous Gameserver', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.ANON_GAMESERVER,
-        'instance':  11245,
-        'accountid': 46124,
-        'format':    'steam3',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.ANON_GAMESERVER,
+        instance:  11245,
+        accountid: 46124,
+        format:    'steam3',
     };
 
     const id = new ID('[A:1:46124:11245]');
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('Steam3ID construction | Lobby', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.CHAT,
-        'instance':  ChatInstanceFlags.Lobby,
-        'accountid': 12345,
-        'format':    'steam3',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.CHAT,
+        instance:  ChatInstanceFlags.Lobby,
+        accountid: 12345,
+        format:    'steam3',
     };
 
     const id = new ID('[L:1:12345]');
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('Steam3ID construction | Lobby Instance', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.CHAT,
-        'instance':  ChatInstanceFlags.Lobby | 55,
-        'accountid': 12345,
-        'format':    'steam3',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.CHAT,
+        instance:  ChatInstanceFlags.Lobby | 55,
+        accountid: 12345,
+        format:    'steam3',
     };
 
     const id = new ID('[L:1:12345:55]');
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('SteamID64 construction | Individual', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.INDIVIDUAL,
-        'instance':  Instance.DESKTOP,
-        'accountid': gaben.accountid,
-        'format':    'steam64',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.INDIVIDUAL,
+        instance:  Instance.DESKTOP,
+        accountid: gaben.accountid,
+        format:    'steam64',
     };
 
     const id = new ID(gaben.id64);
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
 });
 
 test('SteamID64 construction | Clan', (t) => {
-    const should = {
-        'universe':  Universe.PUBLIC,
-        'type':      Type.CLAN,
-        'instance':  Instance.ALL,
-        'accountid': 4,
-        'format':    'steam64',
+    const should: BaseCheckObject = {
+        universe:  Universe.PUBLIC,
+        type:      Type.CLAN,
+        instance:  Instance.ALL,
+        accountid: 4,
+        format:    'steam64',
     };
 
     const id = new ID('103582791429521412');
 
-    const object = {
+    const object: BaseCheckObject = {
         universe:  id.universe,
         type:      id.type,
         instance:  id.instance,
         accountid: id.accountid,
         format:    id.format,
-    }
+    };
 
     t.deepEqual(object, should);
     t.true(id.isValid(), 'ID should be valid');
